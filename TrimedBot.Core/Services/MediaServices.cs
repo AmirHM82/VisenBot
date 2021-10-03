cbeb70719a22c4ae3cc5a7266f0b8634a4683428
Amir Hossein Mousavi <amir138211131812@gmail.com>
2021-10-04 00:24:56 +0330

    Some bullshid changes

--- TrimedBot.Core/Services/MediaServices.cs
+++ TrimedBot.Core/Services/MediaServices.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+//using static System.Linq.Queryable;
 using System.Threading.Tasks;
 using TrimedBot.Core.Interfaces;
 using TrimedBot.DAL.Context;
